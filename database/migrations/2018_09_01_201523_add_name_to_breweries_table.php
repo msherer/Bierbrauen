4b84732e172ddd89a79cb0289d6a1b0398ded540
Myles Forrest <myles@mediotype.com>
2018-09-01 16:26:50 -0400

    Add php docblock comments for human-completed files

--- database/migrations/2018_09_01_201523_add_name_to_breweries_table.php
+++ database/migrations/2018_09_01_201523_add_name_to_breweries_table.php
@@ -4,17 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * Class AddNameToBreweriesTable
+ */
 class AddNameToBreweriesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * Add "name" and "website_url" columns to the "breweries" table.
      *
      * @return void
      */
     public function up()
     {
         Schema::table('breweries', function (Blueprint $table) {
-            //
             $table->string("name");
             $table->string("website_url");
         });
@@ -23,12 +27,13 @@ class AddNameToBreweriesTable extends Migration
     /**
      * Reverse the migrations.
      *
+     * Drop "name" and "website_url" columns from the "breweries" table.
+     *
      * @return void
      */
     public function down()
     {
         Schema::table('breweries', function (Blueprint $table) {
-            //
             $table->dropColumn(["name", "website_url"]);
         });
     }
