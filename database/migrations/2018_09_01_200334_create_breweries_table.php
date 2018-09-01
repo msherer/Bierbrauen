9165d1e74a0c59ec1f4616a0146eec72da81c102
Myles Forrest <myles@mediotype.com>
2018-09-01 16:03:44 -0400

    Artisan make:model Brewery

---
+++ database/migrations/2018_09_01_200334_create_breweries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBreweriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('breweries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('breweries');
+    }
+}
